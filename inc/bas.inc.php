b86db6373c1d6b062870034da95228463fd0cca8
youcef <namoun.youcef.1@gmail.com>
2019-12-22 13:50:39 +0100

    mon premier commit

---
+++ inc/bas.inc.php
@@ -0,0 +1,11 @@
+inc/bas.inc.php
+?
+            </div>
+        </section>
+        <footer>
+            <div class="conteneur">    
+                <?php echo date('Y'); ?> - Tous droits reservés - MonNom MonPrenom.
+            </div>
+        </footer>
+    </body>
+</html>
